3a99c91be2ffa0611acb4acfc240184b4b7f4595
Samuel Folledo <samuelfolledo@gmail.com>
2019-11-06 14:48:51 -0800

    [add] finished math functions for, mean, median, mode, percentile

--- app.py
+++ app.py
@@ -1,52 +1,44 @@
-data = [1, 3, 5, 2, 3, 7, 8, 4, 10, 0, 6, 7, 3, 0, 3, 0, 5, 7, 10, 1, 4, 9, 3]
+import math #needed for rounding up
+
+data = [1, 3, 5, 2, 3, 7, 8, 4, 10, 3, 3, 0, 6, 7, 3, 0, 3, 0, 5, 7, 10, 1, 4, 9]
 
 x_list = [3.3,4.4,5.5,6.71,6.93,4.168,9.779,6.182,7.59,2.167,7.042,10.791,5.313,7.997,5.654,9.27,3.1]
-
-
 y_list = [1.7,2.76,2.09,3.19,1.694,1.573,3.366,2.596,2.53,1.221,2.827,3.465,1.65,2.904,2.42,2.94,1.3]
 
-# print(f"Mean = {mean(data)}")
 
-def mean(list): #gets a list and returns the average a.k.a. central tendency
+def mean(list): #get average
     return(float(sum(list)) / len(list))
-
-def sum(list):
-    total = 0
-    for i in list:
-        total += i
-    return total
 
 def median(list):
     length = len(list)
     sortedData = sorted(list)
-    print(f"Sorted Data = {sortedData}")
     if length < 1: #if length < 0, return None
         return None
-    
-    if length % 2 == 1: #length of list is odd
+    if length % 2 == 1: #if list's length is odd
         return sortedData[length // 2]
-        # return np.median(sortedData)
-    else: #length of list is even, then get the average of the 2 middle
+    else: #if list's length is even, then get the average of the 2 middle
         firstElement = sortedData[length // 2]
         secondElement = sortedData[length // 2 - 1]
-        print(firstElement)
-        print(secondElement)
-#         compute_mean([sortedData[firstElement], sortedData[secondElement]])
-        median = (firstElement + secondElement) / 2
-        return median
+        return (firstElement + secondElement) / 2
 
 def percentile(data, percent):
-    #first we want to sort the data in ascending order
-    # data = np.sort(data)
-    #then we will get the index
+    if percent < 1.0:
+        print("Plese input a percent from 1 - 100 only: ")
+        return None
+    sortedData = sorted(data)    
+    index = (percent/100) * len(data)
+    roundedUpIndex = math.ceil(index)
+    return sortedData[roundedUpIndex-1]
     
-    index = (percent/100)* len(data)
     #we will have to round up to the nearest whole number using the ceiling method and covert to an int
     # index = int(np.ceil(index))
     
-    return data[index-1] #adjust by -1 since indices start with 0
+    # return data[index-1] #adjust by -1 since indices start with 0
 
+print(f"Sorted Data = {sorted(data)}")
 print(f"Sum = {sum(data)}")
 print(f"Length = {len(data)}")
 print(f"Mean = {mean(data)}")
 print(f"Median = {median(data)}")
+
+print(f"Percentile = {percentile(data, 25)}")
