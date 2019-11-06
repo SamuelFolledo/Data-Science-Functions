09d9deb87cd0d1262e8ff5b8b5ec5ad9d016e6ed
Samuel Folledo <samuelfolledo@gmail.com>
2019-11-06 15:24:39 -0800

    [add] incomplete covariance, now outputs the cov(X,Y) only, but not the matrix

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ data = [1, 3, 5, 2, 3, 7, 8, 4, 10, 3, 3, 0, 6, 7, 3, 0, 3, 0, 5, 7, 10, 1, 4, 9
 x_list = [3.3,4.4,5.5,6.71,6.93,4.168,9.779,6.182,7.59,2.167,7.042,10.791,5.313,7.997,5.654,9.27,3.1]
 y_list = [1.7,2.76,2.09,3.19,1.694,1.573,3.366,2.596,2.53,1.221,2.827,3.465,1.65,2.904,2.42,2.94,1.3]
 
-
 def mean(list): #get average
     return(float(sum(list)) / len(list))
 
@@ -29,16 +28,32 @@ def percentile(data, percent):
     index = (percent/100) * len(data)
     roundedUpIndex = math.ceil(index)
     return sortedData[roundedUpIndex-1]
-    
-    #we will have to round up to the nearest whole number using the ceiling method and covert to an int
-    # index = int(np.ceil(index))
-    
-    # return data[index-1] #adjust by -1 since indices start with 0
 
-print(f"Sorted Data = {sorted(data)}")
-print(f"Sum = {sum(data)}")
-print(f"Length = {len(data)}")
-print(f"Mean = {mean(data)}")
-print(f"Median = {median(data)}")
+def covariance(dataX, dataY):
+    result = []
+    length = len(dataX) - 1
+    sortedX = sorted(dataX)
+    sortedY = sorted(dataY)
+    meanX = mean(dataX)
+    meanY = mean(dataY)
 
-print(f"Percentile = {percentile(data, 25)}")
+    total = 0
+    for i in range(len(dataX)):
+        total += (sortedX[i] - meanX) * (sortedY[i] - meanY)
+    total /= length
+
+    print(total)
+
+    print(result)
+
+
+# print(f"Sorted Data = {sorted(data)}")
+# print(f"Sum = {sum(data)}")
+# print(f"Length = {len(x_list)}")
+
+# print(f"Mean = {mean(data)}")
+# print(f"Median = {median(data)}")
+# print(f"Percentile = {percentile(data, 25)}")
+dataX = [1,2,3,4]
+dataY = [1,1,2,3]
+covariance(dataX, dataY)
